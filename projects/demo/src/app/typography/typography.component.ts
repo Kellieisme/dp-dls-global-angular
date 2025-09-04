921e6243c18be6d2312696ce73939cca7b1e4e35
kelliev <40643335+kelliev@users.noreply.github.com>
2025-09-04 15:00:43 -0600

    angular-19 add typography, create PR to main

--- projects/demo/src/app/typography/typography.component.ts
+++ projects/demo/src/app/typography/typography.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-typography',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, CommonModule],
   templateUrl: './typography.component.html',
   styleUrl: './typography.component.scss'
 })
