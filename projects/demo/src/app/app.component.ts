262ccda633f6dbf36422dc94c56e241794ff4431
kellieverne <kellieverne@jeppesen.com>
2025-12-10 15:27:18 -0700

    fix/storybook implement copilot suggestions

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -2,31 +2,31 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ThemeToggleService } from '../../../design/dp-dls-global-angular/theme-toggle/src/theme-toggle.service';
 import { Subscription } from 'rxjs';
 import { MainNavComponent } from "./main-nav/main-nav.component";
+import { Theme } from '../../../design/dp-dls-global-angular/theme-toggle/src/theme-toggle.model';
 
 @Component({
   selector: 'app-root',
-    standalone: true,
-    imports: [MainNavComponent],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+  standalone: true,
+  imports: [MainNavComponent],
+  providers: [ThemeToggleService],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
 
-export class AppComponent  {
+export class AppComponent implements OnInit, OnDestroy {
+  currentTheme!: Theme;
 
   private themeSubscription: Subscription = new Subscription;
 
   constructor(private themeToggleService: ThemeToggleService) {}
 
-  // ngOnInit() {
-  //   // Get the current theme when the component initializes
-  //   this.currentTheme = this.themeToggleService.getCurrentTheme();
-
-  //   // Subscribe to theme changes
-  //   this.themeSubscription = this.themeToggleService.themeChanged$.subscribe((theme: any) => {
-  //     this.currentTheme = theme;
-  //     console.log('Theme changed to:', theme);
-  //   });
-  // }
+  ngOnInit() {
+    // Subscribe to theme changes
+    this.themeSubscription = this.themeToggleService.themeChanged$.subscribe((theme: any) => {
+      this.currentTheme = theme;
+      console.log('Theme changed to:', theme);
+    });
+  }
 
   toggleTheme() {
     // Call the toggleTheme method from the service
